9d5a086b4155e324eb278246fd6ef6a3c9fee8d1
Jesse White <jesse.white6@gmail.com>
2021-04-06 13:11:34 -0700

    completed functionality.
    Co-authored-by: Brent Hubbard <hubbardbrent@hotmail.com>
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>

--- address-book/js/script.js
+++ address-book/js/script.js
@@ -1,11 +1,18 @@
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, emailAddress, homeAddress, workAddress) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phoneNumber = phoneNumber;
+    this.emailAddress = emailAddress;
+    this.additionalAddresses = {}
 }
 
 Contact.prototype.fullName = function () {
     return this.firstName + " " + this.lastName;
+}
+
+Contact.prototype.addAdditionalAddresses = function (address1, address2) {
+    this.additionalAddresses.homeAddress = address1;
+    this.additionalAddresses.workAddress = address2;
 }
 
 function AddressBook() {
@@ -45,11 +52,10 @@ function displayContactDetails(addressBookToDisplay) {
     let htmlForContactInfo = "";
     Object.keys(addressBookToDisplay.contacts).forEach(function (key) {
         const contact = addressBookToDisplay.findContact(key);
-        htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+        htmlForContactInfo += "<li id=" + contact.id + ">" + contact.fullName() + "</li>";
     })
     contactsList.html(htmlForContactInfo);
 };
-
 
 function showContact(contactId) {
     const contact = addressBook.findContact(contactId);
@@ -57,6 +63,9 @@ function showContact(contactId) {
     $(".first-name").html(contact.firstName);
     $(".last-name").html(contact.lastName);
     $(".phone-number").html(contact.phoneNumber);
+    $(".email-address").html(contact.emailAddress);
+    $("#home-address").html(contact.additionalAddresses.homeAddress);
+    $("#work-address").html(contact.additionalAddresses.workAddress);
     let buttons = $("#buttons");
     buttons.empty();
     buttons.append("<button class='deleteButton btn btn-danger' id=" + + contact.id + ">Delete</button>");
@@ -80,15 +89,32 @@ $(document).ready(function () {
         const inputtedFirstName = $("input#new-first-name").val();
         const inputtedLastName = $("input#new-last-name").val();
         const inputtedPhoneNumber = $("input#new-phone-number").val();
+        const inputtedEmail = $("input#new-email-address").val();
+        const inputtedHomeAddress = $("input#new-home-address").val();
+        const inputtedWorkAddress = $("input#new-work-address").val();
 
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
         $("input#new-phone-number").val("");
+        $("input#new-email-address").val("");
+        $("input#new-home-address").val("");
+        $("input#new-work-address").val("");
 
-        let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+        let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, inputtedHomeAddress, inputtedWorkAddress);
         addressBook.addContact(newContact);
+
+        if (inputtedHomeAddress || inputtedWorkAddress) {
+            newContact.addAdditionalAddresses(inputtedHomeAddress, inputtedWorkAddress);
+            $("#home-address").show(newContact.addAdditionalAddresses.inputtedHomeAddress);
+            $("#work-address").show(newContact.addAdditionalAddresses.inputtedWorkAddress);
+        } else if (inputtedHomeAddress) {
+            newContact.addAdditionalAddresses(inputtedHomeAddress);
+            $("#home-address").show(newContact.addAdditionalAddresses.inputtedHomeAddress);
+        } else if (inputtedWorkAddress) {
+            newContact.addAdditionalAddresses(inputtedWorkAddress);
+            $("#work-address").show(newContact.addAdditionalAddresses.inputtedWorkAddress);
+        }
+
         displayContactDetails(addressBook);
-
-
     })
 })
